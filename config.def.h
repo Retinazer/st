84e8f81b7b714d56302251545c9ce50d1b9bd15f
Retinazer <coleyoung@protonmail.com>
2020-08-26 21:48:42 -0400

    Themed for new dotfiles repo

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code:pixelsize=15:antialias=true:autohint=true";
-static int borderpx = 30;
+static char *font = "JetBrains Mono:pixelsize=13:antialias=true:autohint=true";
+static int borderpx = 10;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -43,7 +43,7 @@ int allowaltscreen = 1;
 
 /* frames per second st should at maximum draw to the screen */
 static unsigned int xfps = 120;
-static unsigned int actionfps = 30;
+static unsigned int actionfps = 60;
 
 /*
  * blinking timeout (set to 0 to disable blinking) for the terminal blinking
@@ -66,7 +66,7 @@ const int boxdraw = 1;
 const int boxdraw_bold = 0;
 
 /* braille (U28XX):  1: render as adjacent "pixels",  0: use font */
-const int boxdraw_braille = 1;
+const int boxdraw_braille = 0;
 
 /*
  * bell volume. It must be a value between -100 and 100. Use 0 for disabling
@@ -100,39 +100,47 @@ float alpha = 1.0;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 
-	  /* 8 normal colors */
-  [0] = "#282a2e", /* black   */
-  [1] = "#a54242", /* red     */
-  [2] = "#8c9440", /* green   */
-  [3] = "#de935f", /* yellow  */
-  [4] = "#5f819d", /* blue    */
-  [5] = "#85678f", /* magenta */
-  [6] = "#5e8d87", /* cyan    */
-  [7] = "#707880", /* white   */
+  /* 8 normal colors */
+  [0] = "#2d3741", /* black   */
+  [1] = "#d71a1a", /* red     */
+  [2] = "#57ba37", /* green   */
+  [3] = "#f0d50c", /* yellow  */
+  [4] = "#91aadf", /* blue    */
+  [5] = "#cf73e6", /* magenta */
+  [6] = "#b7cbf4", /* cyan    */
+  [7] = "#dedede", /* white   */
 
   /* 8 bright colors */
-  [8]  = "#373b41", /* black   */
-  [9]  = "#cc6666", /* red     */
-  [10] = "#b5bd68", /* green   */
-  [11] = "#f0c674", /* yellow  */
-  [12] = "#81a2be", /* blue    */
-  [13] = "#b294bb", /* magenta */
-  [14] = "#8abeb7", /* cyan    */
-  [15] = "#c5c8c6", /* white   */
+  [8]  = "#666c74", /* black   */
+  [9]  = "#ff0000", /* red     */
+  [10] = "#d8fa3b", /* green   */
+  [11] = "#e7c547", /* yellow  */
+  [12] = "#b7cbf4", /* blue    */
+  [13] = "#b77ee0", /* magenta */
+  [14] = "#a9c1de", /* cyan    */
+  [15] = "#eeeeee", /* white   */
 
   /* special colors */
-  [256] = "#1d1f21", /* background */
-  [257] = "#c5c8c6", /* foreground */
+  [256] = "#1a2026", /* background */
+  [257] = "#d0d0d0", /* foreground */
 };
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
+ * foreground, background, cursor
  */
- unsigned int defaultbg = 256;
- unsigned int defaultfg = 257;
- unsigned int defaultcs = 257;
- unsigned int defaultrcs= 9;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 257;
+unsigned int defaultrcs = 8;
+
+/*
+ * Colors used, when the specific fg == defaultfg. So in reverse mode this
+ * will reverse too. Another logic would only make the simple feature too
+ * complex.
+ */
+unsigned int defaultitalic = 7;
+unsigned int defaultunderline = 7;
 
 /*
  * Default shape of cursor
